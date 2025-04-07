17f9cc70c50f4758374594af9f65132c648c79c9
QSI-Official <149574660+QSI-Official@users.noreply.github.com>
2025-04-07 18:05:39 +0200

    Bugfix: urls.py & Fix: views.py includes all urls

--- NZC/urls.py
+++ NZC/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), #TODO: add index page
-    path('about/', views.about, name='about'), #TODO: add about page
-    path('pdf/', views.pdf, name='pdf'), #TODO: add pdf page
-    path('manual/', views.manual, name='manual'), #TODO: add manual page
-    path('results/', views.results, name='results'), #TODO: add results page
+    path('about', views.about, name='about'), #TODO: add about page
+    path('pdf', views.pdf, name='pdf'), #TODO: add pdf page
+    path('manual', views.manual, name='manual'), #TODO: add manual page
+    path('results', views.results, name='results'), #TODO: add results page
 ]
--- NZC/views.py
+++ NZC/views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def pdf(request):
+    return render(request, 'pdf.html')
+
+def manual(request):
+    return render(request, 'manual.html')
+
+def results(request):
+    if request.method == 'POST' and request.FILES['file']:   
+        results = f"you uploaded {request.FILES.get('file')}, and you should reduce your carbon footprint by x and y"
+        context = {
+            'results': results,
+        }
+    else:
+        context = {
+            'results': None,
+        }
+    return render(request, 'results.html', context)
