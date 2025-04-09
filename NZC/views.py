5731db5c9b9cb70fb2d593420bc882f9789fc52a
QSI-Official <149574660+QSI-Official@users.noreply.github.com>
2025-04-09 16:47:44 +0200

    Bugfix: only allow pdf uploads, else display error

--- NZC/views.py
+++ NZC/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
@@ -15,14 +17,24 @@ def manual(request):
 
 def results(request):
     if request.method == 'POST':
+
         if request.FILES.get('file'):
+            pdf_file = request.FILES.get('file')
+
+            if not pdf_file.name.endswith('.pdf'):
+                messages.error(request, 'File is not PDF type')
+                return redirect('index')
+
             results = f"you uploaded {request.FILES.get('file')}, and you should reduce your carbon footprint by x and y"
             context = {
                 'results': results
             }
+
         elif request.POST.get('scope1') and request.POST.get('scope2') and request.POST.get('scope3') and request.POST.get('profit'):
+            
             scopes = [request.POST.get('scope1'), request.POST.get('scope2'), request.POST.get('scope3')]
             profit = request.POST.get('profit')
+            
             results = f"you selected {scopes[0]} and {scopes[1]} and {scopes[2]} and profit {profit}, and you should reduce your carbon footprint by x and y"
             context = {
                 'results': results
