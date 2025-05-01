583a42b2441034b6d3fe24ae104daccf56dbbb0f
QSI-Official <149574660+QSI-Official@users.noreply.github.com>
2025-05-01 09:37:47 +0200

    chore: remove old TODO comment

--- NZC/urls.py
+++ NZC/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
-    path('pdf', views.pdf, name='pdf'), #TODO: add pdf page? confimation of data extracted from the pdf before submission maybe?
+    path('pdf', views.pdf, name='pdf'),
     path('manual', views.manual, name='manual'),
     path('results', views.results, name='results'),
     path('ccs_methods', views.ccs_methods, name='ccs_methods'),
