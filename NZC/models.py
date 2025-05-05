212b7ed9bf48ea3c7e95fb14120b33f630c140bb
QSI-Official <149574660+QSI-Official@users.noreply.github.com>
2025-05-05 11:13:19 +0200

    Database: create result model, pre-migration

--- NZC/models.py
+++ NZC/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Result(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    scope1 = models.IntegerField(max_length=20)
+    scope2 = models.IntegerField(max_length=20)
+    scope3 = models.IntegerField(max_length=20)
+    profit = models.IntegerField(max_length=20)
+
+    pdfname = models.CharField(max_length=100, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(max_length=100, null=True)
